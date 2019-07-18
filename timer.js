6765d4b184f6bf25cbc6572d73570152006a4b41
snicklefrittz <51767785+snicklefrittz@users.noreply.github.com>
2019-07-18 13:19:12 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Aug 2, 2019 00:00:00").getTime();
+var countDownDate2 = new Date("Jan 1, 2020 00:00:00").getTime(); //just added
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -9,21 +10,34 @@ var x = setInterval(function() {
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
+  var distance2 = countDownDate2 - now; //just added
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes and seconds for Chipotle
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  //added all of these
+  
+   var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+  var hours2 = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  
+  //added it
+  
+  document2.getElementById("demo2").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Good Bye Currnet Contract";
   }
 }, 1000);
 
@@ -33,3 +47,5 @@ function blinktext() {
     f.style.visibility = (f.style.visibility == 'hidden' ? '' : 'hidden');
   }, 1000);
 }
+
+
